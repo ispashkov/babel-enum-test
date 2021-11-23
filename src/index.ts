a9b862fce6058e423be0d4e08887fb96a62eea6f
Ilya Pashkov <ispashkov@gmail.com>
2021-11-23 12:05:59 +0400

    first commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+export enum Test {
+    A = 'a',
+    B = 'b'
+}
+
+export const enum TestConst {
+    C = 'c',
+    D = 'd'
+}
+
+export const test: Test = Test.A
+
+export const testConst: TestConst = TestConst.C
